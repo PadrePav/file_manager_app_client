0195894570b67d88bbb489cfb7ed7400b3ba4070
Padre <kuharukpavel@gmail.com>
2023-05-28 13:47:23 +0300

    Adding log out simulation

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,16 +3,23 @@ import {AppBar, Box, Toolbar} from "@mui/material";
 import {Link, Outlet} from "react-router-dom";
 
 const Layout = () => {
-
+  const style = {
+    textDecoration: 'none',
+    color: '#ffffff'
+  }
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" color="primary" sx={{padding: 0}}>
           <Toolbar sx={{justifyContent: 'space-between', padding: 0}}>
-            <Link to={'/user/root'} style={{textDecoration: 'none', color: '#ffffff'}}>My space</Link>
-            <Toolbar style={{width: '120px', justifyContent: 'space-between', padding: 0}}>
-              <Link to={'/sign-in'} style={{textDecoration: 'none', color: '#ffffff'}}>Sign-in</Link>
-              <Link to={'/sign-up'} style={{textDecoration: 'none', color: '#ffffff'}}>Sign-up</Link>
+            <Link to={'/user/root'} style={style}>My space</Link>
+            <Toolbar style={{justifyContent: 'space-between', padding: 0}}>
+              <Link to={'/sign-in'} style={style} onClick={() => {
+                sessionStorage.removeItem('userName')
+                sessionStorage.removeItem('token')
+              }}>
+                Log out
+              </Link>
             </Toolbar>
           </Toolbar>
         </AppBar>
